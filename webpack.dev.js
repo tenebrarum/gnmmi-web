1cc23184d6085a6e034ea161891e356f80cc8415
Chris <tenebrarum91@gmail.com>
2021-09-04 22:59:39 +0200

    chore: upgrade versions

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: './dist',
+    },
     historyApiFallback: true,
   },
   devtool: 'inline-source-map',
